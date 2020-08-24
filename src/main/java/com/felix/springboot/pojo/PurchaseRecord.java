5a8e0af028cc94e683705e92b79cbc75b491f69a
Felix <742863722@qq.com>
2020-08-24 23:00:31 +0800

    supplement pojo

---
+++ src/main/java/com/felix/springboot/pojo/PurchaseRecord.java
@@ -0,0 +1,84 @@
+package com.felix.springboot.pojo;
+
+import org.apache.ibatis.type.Alias;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+@Alias("purchaseRecord")
+public class PurchaseRecord implements Serializable {
+
+    private static final long serialVersionUID = -457171040510870265L;
+    private Long id;
+    private Long userId;
+    private Long productId;
+    private double price;
+    private int quantity;
+    private double sum;
+    private Timestamp purchaseTime;
+    private String note;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getSum() {
+        return sum;
+    }
+
+    public void setSum(double sum) {
+        this.sum = sum;
+    }
+
+    public Timestamp getPurchaseTime() {
+        return purchaseTime;
+    }
+
+    public void setPurchaseTime(Timestamp purchaseTime) {
+        this.purchaseTime = purchaseTime;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
